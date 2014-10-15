ee4999e277c2474a8aee38522b1d45dada40845a
Sawada Kensuke <sasasawada@gmail.com>
2014-10-15 15:06:07 +0900

    add lambda.c

---
+++ src/type/lambda.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+const void* Type = &Lambda;
+
+typedef struct
+{
+  
+} Data;
+
+static bool release(Object* lambda)
+{
+  return true;
+}
+
+static void apply(Object* lambda, void (*proc)(Object*))
+{
+
+}
+
+static Object* new(Object* meta, Object* param_list, 
+		   Object** exps, int len)
+{
+
+
+}
+
+static Object* makeForm(Object* meta, Object* env,
+			Object** args, int argc)
+{
+
+
+}
+
+
+t_Lambda Lambda = {
+  {release, apply, NULL, NULL},
+  new, makeForm
+};
