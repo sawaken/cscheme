d7d564653fdfeac6a404c5bf0029dc0f946b330e
Sawada Kensuke <sasasawada@gmail.com>
2014-10-16 09:50:53 +0900

    add test_exception.cc

---
+++ unit_test/type/test_exception.cc
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <type/type.h>
+#include <iostream>
+
+TEST(take, case1)
+{
+  Object* meta = MetaObject.New(10);
+  Object* dummy = Dummy.New(meta, NULL);
+  Object* ex = Exception.New(meta, dummy);
+
+  ASSERT_EQ(dummy, Exception.take(ex));
+
+  MetaObject.release(ex);
+  
+  ASSERT_TRUE(Dummy.isReleased(dummy));
+}
